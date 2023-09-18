d60e8aa1a089167ac78be74c9612b19eff5aa08a
AnnKlii <anna_k84@maail.ru>
2023-09-18 20:13:11 +0300

    add week11

---
+++ todolist/script.js
@@ -0,0 +1,20 @@
+const addTask = document.querySelector('.list__input');
+const addButton = document.querySelector('.list__btn');
+const list = document.querySelector('.list__list');
+
+addButton.addEventListener('click',
+    function () {
+        const taskText = addTask.value;
+        const newTask = document.createElement('li');
+        newTask.innerHTML = taskText;
+        list.append(newTask);
+        addTask.value = "";
+    }
+);
+
+list.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    }
+});
+
